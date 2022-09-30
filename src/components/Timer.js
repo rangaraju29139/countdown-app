fd73e9df258bc82b28f373041721eec34d448c18
rangaraju29139 <rangaraju29139@gmail.com>
2022-10-01 05:16:34 +0530

    added the button and raw html contents before start

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,7 +4,38 @@ import "../styles/styles.css";
 function Timer() {
   return (
     <>
-      <section className="timer">Timer</section>
+      <section className="timer">
+        <div className="timer-row-1">
+          <i
+            style={{ color: "white" }}
+            className="fa-solid fa-up-right-and-down-left-from-center"
+          ></i>
+        </div>
+        <div className="timer-row-2">
+          <div className="timer-time" id="time">
+            <span>00:00:10</span>
+          </div>
+        </div>
+        <div className="timer-row-3 timer-controls">
+          <div className="before-start">
+            <div>
+              <button id="start" className="before-start-button" name="start">
+                Start
+              </button>
+            </div>
+            <div>
+              <button id="increase-time" className="before-start-button">
+                +
+              </button>
+            </div>
+            <div>
+              <button id="decrease-time" className="before-start-button">
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
