f32b13e0eb00c46da371b025e16c1820a4d726ca
rangaraju29139 <rangaraju29139@gmail.com>
2022-10-01 04:12:29 +0530

    added the logo and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../styles/styles.css";
+import headerImg from "../assets/images/timer-icon.png";
 
 function Header() {
   return (
     <>
-      <section className="header">Header</section>
+      <section className="header">
+        <div className="header-left">
+          <div className="header-img ">
+            <img src={headerImg} alt="header-img"></img>
+          </div>
+          <div className="header-name">CountDown App</div>
+        </div>
+      </section>
     </>
   );
 }
